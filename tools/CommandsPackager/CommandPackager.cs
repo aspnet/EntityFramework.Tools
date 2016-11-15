c5936283a5abe2192c16e3c63821ce738e881256
Brice Lambson <bricelam@microsoft.com>
2016-11-15 14:10:15 -0800

    Cleanup dependencies

--- tools/CommandsPackager/CommandPackager.cs
+++ tools/CommandsPackager/CommandPackager.cs
@@ -96,13 +96,13 @@ namespace CommandPackager
                 return Environment.GetEnvironmentVariable("KOREBUILD_NUGET_EXE");
             }
 
-            var nugetPath = Path.Combine(_baseDir, ".build", "nuget.3.5.0-rc1.exe");
+            var nugetPath = Path.Combine(_baseDir, ".build", "nuget.3.5.0.exe");
             if (File.Exists(nugetPath))
             {
                 return nugetPath;
             }
-            Console.WriteLine("log : Downloading nuget.exe 3.5.0-rc1");
-            var response = await new HttpClient().GetAsync("https://dist.nuget.org/win-x86-commandline/v3.5.0-rc1/NuGet.exe");
+            Console.WriteLine("log : Downloading nuget.exe 3.5.0");
+            var response = await new HttpClient().GetAsync("https://dist.nuget.org/win-x86-commandline/v3.5.0/NuGet.exe");
             using (var file = new FileStream(nugetPath, FileMode.CreateNew))
             {
                 response.EnsureSuccessStatusCode();
