0852cfd0218051eeb420de322e76fd022c02b864
Brice Lambson <bricelam@outlook.com>
2016-12-19 21:27:12 -0800

    Remove unused string.Formst

--- src/ef/AnsiTextWriter.cs
+++ src/ef/AnsiTextWriter.cs
@@ -17,9 +17,9 @@ namespace Microsoft.EntityFrameworkCore.Tools
             _writer = writer;
         }
 
-        public void WriteLine(string format, params object[] args)
+        public void WriteLine(string text)
         {
-            Interpret(string.Format(format, args));
+            Interpret(text);
             _writer.Write(Environment.NewLine);
         }
 
